0c9aa2ea719af1c5bc938d32d2d9fdbaa0ab273e
Tok-Amak <stev-pat@hotmail.fr>
2023-12-05 09:50:31 +0100

    Changemement chemind d'acces

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/Portfolio" element={<Home />} />
           <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
